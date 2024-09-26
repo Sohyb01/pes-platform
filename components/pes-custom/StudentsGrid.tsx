174837edd1a897d1a6f7796dbbeaa588556ff681
Sohyb01 <sohyb0155@gmail.com>
2024-09-27 00:10:21 +0200

    fixing bugs 2

--- components/pes-custom/StudentsGrid.tsx
+++ components/pes-custom/StudentsGrid.tsx
@@ -1,21 +1,6 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
-
-const fadeInAnimationVariants = {
-  initial: {
-    opacity: 0,
-    y: 100,
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.05 * index,
-    },
-  }),
-};
 
 const StudentsGrid = () => {
   return (
