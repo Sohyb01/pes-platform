1baf126e1b35baddafa880db8e1207432292e59e
Sohyb01 <sohyb0155@gmail.com>
2024-11-03 15:35:52 +0200

    removing non existent imports

--- app/dashboard/superadmin/franchises/page.tsx
+++ app/dashboard/superadmin/franchises/page.tsx
@@ -1,21 +1,17 @@
-import { exampleColumns } from "@/components/pes-custom/table-columns/classesColumns";
-import { DataTable } from "@/components/ui/data-table";
-import { exampleData } from "@/lib/data";
-import { exampleObject } from "@/lib/types-backend-data";
-
-async function getData(): Promise<exampleObject[]> {
+async function getData(): Promise<null> {
   // Fetch data from your API here.
   // Must fit the type definition to be inserted into the table
-  return exampleData;
+  return null;
 }
 
 export default async function ExampleDashboardPage() {
   const data = await getData();
 
+  console.log(data);
+
   return (
     <div className="dashboard-tab-wrapper">
-      <h3 className="text-h3">Assignments</h3>
-      <DataTable columns={exampleColumns} data={data} />
+      <h3 className="text-h3">Franchises</h3>
     </div>
   );
 }
