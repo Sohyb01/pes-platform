11c141b0beb255f3ce64c72bc66d03aed5f9a3aa
Sohyb01 <sohyb0155@gmail.com>
2024-12-13 00:58:03 +0200

    .

--- app/dashboard/student/classes/page.tsx
+++ app/dashboard/student/classes/page.tsx
@@ -3,8 +3,8 @@ import { ChatLayout } from "@/components/pes-custom/platform-components/chat-lay
 
 const page = () => {
   return (
-    <div className="dashboard-tab-wrapper h-full">
-      <div className="h-full w-full bg-background overflow-y-scroll rounded-[1rem] max-w-[50%]">
+    <div className="dashboard-tab-wrapper p-0 h-full">
+      <div className="h-full w-full bg-background overflow-y-scroll rounded-[1rem] lg:max-w-[50%]">
         <ChatLayout defaultLayout={undefined} navCollapsedSize={8} />
       </div>
     </div>
