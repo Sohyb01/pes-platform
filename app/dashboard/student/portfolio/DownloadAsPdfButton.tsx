4717434ab9441c35f05c9420451f655bdd75faae
boodyf2 <abdalrahman.vim@gmail.com>
2024-12-23 23:18:14 +0200

    removed html2pdf

--- app/dashboard/student/portfolio/DownloadAsPdfButton.tsx
+++ app/dashboard/student/portfolio/DownloadAsPdfButton.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Download } from "lucide-react";
-import html2pdf from "html2pdf.js";
 
 const DownloadAsPdfButton = ({ className }: { className?: string }) => {
   const handleOnClick = () => {
     const portfolio = document.getElementById("portfolio");
-    html2pdf(portfolio);
   };
 
   return (
